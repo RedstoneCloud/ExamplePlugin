94f6ef2980712270ee02ceeabafe53c624342096
tunikakeks <keksdev9513@gmail.com>
2024-09-22 14:43:40 +0000

    Added jitpack repo

--- src/main/java/de/redstonecloud/Main.java
+++ src/main/java/de/redstonecloud/Main.java
@@ -1,16 +1,11 @@
 package de.redstonecloud;
 
-import de.redstonecloud.cloud.events.defaults.ServerStartEvent;
 import de.redstonecloud.cloud.plugin.Plugin;
 
 public class Main extends Plugin {
     @Override
     public void onEnable() {
         System.out.println("ExamplePlugin enabled!");
-
-        getCloud().getEventManager().subscribe(ServerStartEvent.class, (e) -> {
-            System.out.println("Server started: " + e.getServerName());
-        });
     }
 
     @Override
